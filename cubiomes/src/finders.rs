a44d3c04c7865d4e5f25878d97c02b4e080ec3a2
0xTas <admin@0xtas.dev>
2023-08-07 16:26:27 -0700

    Improved ergonomics of the BiomeCache API

--- cubiomes/src/finders.rs
+++ cubiomes/src/finders.rs
@@ -5,7 +5,6 @@ pub use libcubiomes_sys::BiomeID;
 pub use libcubiomes_sys::Dimension;
 pub use libcubiomes_sys::MCVersion;
 
-
 pub struct CubiomesFinder {
     generator: MaybeUninit<libcubiomes_sys::Generator>,
 }
@@ -17,18 +16,14 @@ impl CubiomesFinder {
                 generator: MaybeUninit::zeroed(),
             };
             libcubiomes_sys::setupGenerator(finder.generator.as_mut_ptr(), version as c_int, 0);
-            libcubiomes_sys::applySeed(
-                finder.generator.as_mut_ptr(),
-                dim,
-                seed as u64,
-            );
+            libcubiomes_sys::applySeed(finder.generator.as_mut_ptr(), dim, seed as u64);
             finder
         }
     }
 
     pub fn apply_seed(&mut self, seed: i64, dim: Dimension) {
         unsafe {
-            libcubiomes_sys::applySeed(self.generator.as_mut_ptr(), dim, seed as u64);   
+            libcubiomes_sys::applySeed(self.generator.as_mut_ptr(), dim, seed as u64);
         }
     }
 
@@ -82,53 +77,52 @@ pub struct BiomeCache {
 }
 
 impl BiomeCache {
-    #[allow(clippy::too_many_arguments)]
     pub fn new(
         finder: &CubiomesFinder,
         scale: CoordScaling,
-        x: i32,
-        z: i32,
-        sx: i32,
-        sz: i32,
-        y: i32,
-        sy: i32,
+        range_start: (i32, i32, i32), // (x, y, z)
+        range_size: (i32, i32, i32),  // (sx, sy, sz)
     ) -> Self {
         unsafe {
             let r = libcubiomes_sys::Range {
                 scale: scale.value(), // Divide your input coordinates by this value
                 // Define the position and size for a horizontal area:
-                x,
-                z,  // position (x,z)
-                sx, // size (width,height)
-                sz,
-                // Set the vertical range as a plane near sea level at scale 1:4 (unless 1:1).
-                y,
-                sy,
+                x: range_start.0, // position (x, y, z)
+                y: range_start.1,
+                z: range_start.2,
+                sx: range_size.0, // size (width, vertical range, height)
+                sy: range_size.1, // Set the vertical range as a plane near sea level at scale 1:4 (unless Coordscaling == Block [1:1]).
+                sz: range_size.2,
             };
             let biome_id_cache: *mut BiomeID =
                 libcubiomes_sys::allocCache(finder.generator.as_ptr(), r);
             libcubiomes_sys::genBiomes(finder.generator.as_ptr(), biome_id_cache, r);
             BiomeCache {
                 biome_id_cache,
-                x,
-                y,
-                z,
-                sx,
-                sy,
-                sz,
+                x: range_start.0,
+                y: range_start.1,
+                z: range_start.2,
+                sx: range_size.0,
+                sy: range_size.1,
+                sz: range_size.2,
             }
         }
     }
 
     pub fn is_in_bounds(&self, x: i32, y: i32, z: i32) -> bool {
-        x >= self.x && x < self.x + self.sx && y >= self.y && y < self.y + self.sy && z >= self.z && z < self.z + self.sz
+        x >= self.x
+            && x < self.x + self.sx
+            && y >= self.y
+            && y < self.y + self.sy
+            && z >= self.z
+            && z < self.z + self.sz
     }
 
     pub fn get_biome_at(&self, x: i32, y: i32, z: i32) -> BiomeID {
         if !self.is_in_bounds(x, y, z) {
             panic!(
-                "Coordinate out of range for cache! Accepted: (x:{}..{}, z:{}..{}), Received: (x:{}, z:{}).",
-                    self.x, self.x + self.sx, self.z, self.z + self.sz, x, z
+                "Coordinate out of range for cache! Accepted: (x:{}..{}, y:{}..{}, z:{}..{}), Received: (x:{}, y:{}, z:{}).",
+                    self.x, self.x + self.sx, self.y, self.y + self.sy, self.z, self.z + self.sz, x, y, z
             );
         }
         let i_x = x - self.x;
